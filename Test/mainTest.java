44e63034ebbd2b2ea0f8a08661e08b92dcfbc39d
George Thomas Alexander <alexagt0@sewanee.edu>
2024-08-13 13:19:01 -0500

    Added test stuff

--- Test/mainTest.java
+++ Test/mainTest.java
@@ -1,3 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class mainTest {
 
+    private Logic logic;
+
+    @BeforeEach
+    public void setUp(){
+        logic = new Logic();
+    }
+
+    @Test
+    public void templateTest(){
+
+    }
 }
