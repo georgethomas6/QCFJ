0109824dcc33dfc20db35e755fabd018302cbf62
George Thomas Alexander <alexagt0@sewanee.edu>
2024-08-14 14:03:48 -0500

    finished testing

--- Test/measureTests.java
+++ Test/measureTests.java
@@ -42,5 +42,86 @@ public class measureTests {
     }
 
 
+    @Test
+    public void testMeasure(){
+        logic.updateGameState(1, 2);
+        logic.updateGameState(1, 1);
+        logic.updateGameState(1, 1);
+        logic.gameStateToBoard();
+        logic.setMoveStates("HCC");
+        logic.measure();
+        logic.gameStateToBoard();
+        ArrayList<String> expectedOne = new ArrayList<>();
+        ArrayList<String> expectedTwo = new ArrayList<>();
+        expectedOne.add("111");
+        expectedTwo.add("211");
+        boolean worked = logic.getGameState().equals(expectedOne) || logic.getGameState().equals(expectedTwo);
+        assertTrue(worked);
+
+        ArrayList<String> gameState = new ArrayList<>();
+        gameState.add("111");
+        logic.setGameState(gameState);
+        logic.setMoveStates("CCC");
+        logic.gameStateToBoard();
+        logic.measure();
+        assertEquals(logic.getGameState(), gameState);
+        gameState.clear();
+
+
+        logic.updateGameState(1, 2);
+        logic.updateGameState(2, 5);
+        logic.setMoveStates("HV");
+        logic.gameStateToBoard();
+        logic.handleEntanglement(2, 5);
+        logic.gameStateToBoard();
+        logic.updateGameState(2, 2);
+        logic.setMoveStates("HVC");
+        logic.measure();
+        ArrayList<String> potentialOne = new ArrayList<>();
+        ArrayList<String> potentialTwo = new ArrayList<>();
+        potentialTwo.add("222");
+        potentialOne.add("152");
+        boolean itWorked = logic.getGameState().equals(potentialOne) || logic.getGameState().equals(potentialTwo);
+        assertTrue(itWorked);
+
+        logic.setGameState(new ArrayList<String>());
+        logic.updateGameState(1, 2);
+        logic.updateGameState(2, 5);
+        logic.setMoveStates("VH");
+        logic.gameStateToBoard();
+        logic.handleEntanglement(2, 5);
+        logic.gameStateToBoard();
+        logic.updateGameState(2, 2);
+        logic.setMoveStates("VHC");
+        logic.measure();
+        ArrayList<String> potentialThree = new ArrayList<>();
+        ArrayList<String> potentialFour = new ArrayList<>();
+        potentialThree.add("122");
+        potentialFour.add("252");
+        boolean itWorks = logic.getGameState().equals(potentialThree) || logic.getGameState().equals(potentialFour);
+        assertTrue(itWorks);
+
+        logic.setGameState(new ArrayList<String>());
+        logic.updateGameState(1, 2);
+        logic.updateGameState(2, 5);
+        logic.setMoveStates("VH");
+        logic.gameStateToBoard();
+        logic.handleEntanglement(2, 5);
+        logic.gameStateToBoard();
+        logic.updateGameState(2, 3);
+        logic.setMoveStates("VHH");
+        logic.measure();
+        ArrayList<String> potentialFive = new ArrayList<>();
+        ArrayList<String> potentialSix = new ArrayList<>();
+        potentialFive.add("123");
+        potentialFive.add("122");
+        potentialSix.add("252");
+        potentialSix.add("253");
+        boolean itWork = logic.getGameState().equals(potentialFive) || logic.getGameState().equals(potentialSix);
+        assertTrue(itWork);
+    }
+
+
+
 
 }
